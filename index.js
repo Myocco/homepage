2d3e83b8755fd0e9544ca45b83eb4ee09c518864
Mia <miayocco@gmail.com>
2022-01-30 22:13:02 -0500

    added javascript

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
+console.log('Hello World');
 
-function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName);
-}
+//let name = 'Mosh'; //declare variable
+//Can't be a reserved keyword
+//Can't start with a number
+//Can't contain a space or -
+//ARE case-sensitive
+//console.log(name);
 
-function square(number) {
-   return number * number;
-}
+//const interestRate = 0.3; //declares a constant
+//console.log(interestRate);
 
-console.log(square(2));
+//Primative Types
+//let name = 'Mosh'; //string literal
+//let age = 30; //number literal
+//let isApproved = true; //boolean literal
+//let firstName = undefined;
+//let selectedColor = null; //no selection or clear value
+
+//Reference Types
+
+//objects
+//let person = {
+//   name: 'Mosh',
+//    age: 30
+//}; //object literal (curly brackets)
+//dot notation
+//person.name = 'John';
+//bracket notation
+//person['name'] = 'Mary'
+//console.log(person.name);
+
+//arrays
+//let selectedColors = ['red','blue'];//array literal (square brackets)
+//selectedColors[2] = '1';//adds to selected colors in console
+//console.log(selectedColors[0]);//selects a specific element by index #
+//console.log(selectedColors.length);//will output number of elements
+
+//functions
+//function greet(name, lastName) {
+//    console.log('Hello ' + name + ' ' + lastName);
+//}
+//greet('John', 'Smith');//calls function
+
+//function square(number) {
+//    return number * number;
+//}
+//console.log(square(2));
